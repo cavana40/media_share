188fdcd265d899d5b4ea07f465deb24b02a8c9bd
Raghu Betina <raghu@firstdraft.com>
2019-12-01 20:08:03 +0000

    Direct association between users and shows

--- app/models/show.rb
+++ app/models/show.rb
@@ -1,6 +1,9 @@
 class Show < ApplicationRecord
   # Direct associations
 
+  belongs_to :users,
+             :class_name => "User"
+
   # Indirect associations
 
   # Validations
--- app/models/user.rb
+++ app/models/user.rb
@@ -1,6 +1,10 @@
 class User < ApplicationRecord
   # Direct associations
 
+  has_many   :shows,
+             :foreign_key => "users_id",
+             :dependent => :destroy
+
   has_many   :books,
              :foreign_key => "users_id",
              :dependent => :destroy
