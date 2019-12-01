8148455dc056f8e58d0d5d3c28bcd9350afe49c1
Raghu Betina <raghu@firstdraft.com>
2019-12-01 20:09:43 +0000

    Only film user can modify film

--- app/controllers/films_controller.rb
+++ app/controllers/films_controller.rb
@@ -1,4 +1,14 @@
 class FilmsController < ApplicationController
+  before_action :current_user_must_be_film_users, :only => [:edit_form, :update_row, :destroy_row]
+
+  def current_user_must_be_film_users
+    film = Film.find(params["id_to_display"] || params["prefill_with_id"] || params["id_to_modify"] || params["id_to_remove"])
+
+    unless current_user == film.users
+      redirect_to :back, :alert => "You are not authorized for that."
+    end
+  end
+
   def index
     @films = Film.all
 
