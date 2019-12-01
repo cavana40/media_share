6cfe25adf8318804ebf4cdf6ec652cf4d77faaf5
Raghu Betina <raghu@firstdraft.com>
2019-12-01 20:07:49 +0000

    Generate follow_requests resource with draft:resource

---
+++ db/migrate/20191201200749_create_follow_requests.rb
@@ -0,0 +1,10 @@
+class CreateFollowRequests < ActiveRecord::Migration[5.1]
+  def change
+    create_table :follow_requests do |t|
+      t.integer :recipient_id
+      t.integer :sender_id
+
+      t.timestamps
+    end
+  end
+end
