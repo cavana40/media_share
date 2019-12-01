69d283558d5f0353609ee6b9abc095228c69e646
Raghu Betina <raghu@firstdraft.com>
2019-12-01 20:08:03 +0000

    Direct association between users and podcasts

--- app/models/user.rb
+++ app/models/user.rb
@@ -1,6 +1,10 @@
 class User < ApplicationRecord
   # Direct associations
 
+  has_many   :podcasts,
+             :foreign_key => "users_id",
+             :dependent => :destroy
+
   has_many   :articles,
              :foreign_key => "users_id",
              :dependent => :destroy
