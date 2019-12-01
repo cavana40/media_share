d8da29011edcd37eb5e0ac171bb149716c4c3c4d
Raghu Betina <raghu@firstdraft.com>
2019-12-01 20:09:43 +0000

    Only show user can modify show

--- app/controllers/shows_controller.rb
+++ app/controllers/shows_controller.rb
@@ -1,4 +1,14 @@
 class ShowsController < ApplicationController
+  before_action :current_user_must_be_show_users, :only => [:edit_form, :update_row, :destroy_row]
+
+  def current_user_must_be_show_users
+    show = Show.find(params["id_to_display"] || params["prefill_with_id"] || params["id_to_modify"] || params["id_to_remove"])
+
+    unless current_user == show.users
+      redirect_to :back, :alert => "You are not authorized for that."
+    end
+  end
+
   def index
     @shows = Show.all
 
