1bb2d4cf0e2a49ec9fea7ed7f739e64b71098c36
Raghu Betina <raghu@firstdraft.com>
2019-12-01 20:09:43 +0000

    Only book user can modify book

--- app/controllers/books_controller.rb
+++ app/controllers/books_controller.rb
@@ -1,4 +1,14 @@
 class BooksController < ApplicationController
+  before_action :current_user_must_be_book_users, :only => [:edit_form, :update_row, :destroy_row]
+
+  def current_user_must_be_book_users
+    book = Book.find(params["id_to_display"] || params["prefill_with_id"] || params["id_to_modify"] || params["id_to_remove"])
+
+    unless current_user == book.users
+      redirect_to :back, :alert => "You are not authorized for that."
+    end
+  end
+
   def index
     @books = Book.all
 
